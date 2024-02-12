74f3e0dd8c97d793183346c4c475eb6df53d7b4f
callmecposu <pc.cposu@gmail.com>
2024-02-12 17:10:40 -0500

    #client: added a custom theme

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,25 +1,45 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
     },
-  },
-  plugins: [require('daisyui')],
-  daisyui:{
-    themes:[
-      'cmyk'
-    ]
-  }
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#f7c873",
+
+                    secondary: "#faebcd",
+
+                    accent: "#434343",
+
+                    neutral: "#ffffff",
+
+                    "base-100": "#f8f8f8",
+
+                    info: "#d2e0fb",
+
+                    success: "#b7e5b4",
+
+                    warning: "#ff9843",
+
+                    error: "#ff6868",
+                },
+            },
+        ],
+    },
 };
 export default config;
