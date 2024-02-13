1e63cf2c162d5b5ed32129e11579bdb01cde83b3
Vlad Huziienko <guzienko.vlad@gmail.com>
2024-02-13 11:21:25 -0500

    Spending Record Generic Search Added

--- server/services/spendingRecord.py
+++ server/services/spendingRecord.py
@@ -1,5 +1,6 @@
 from mongoengine import *
 from models import spendingRecord as SpendingRecordModel
+import re
 
 def addSpendingRecord(source, date, amount, note, category, tags, userId):
     newSpendingRecord = SpendingRecordModel.SpendingRecord(
@@ -24,3 +25,109 @@ def editSpendingRecord(id, source, date, amount, note, category, tags, userId):
     spendingRecord.tags = tags
     spendingRecord.save()
     return spendingRecord.to_json()
+
+def search(userId, query, dateRange, amountRange, category, sorting):
+    pipeline = []
+
+    if query:
+        regex_query = re.compile(query, re.IGNORECASE)
+        pipeline.append(
+            {
+                '$match': {
+                    '$or': [
+                        {
+                            'source': {
+                                '$regex': regex_query
+                            }
+                        },
+                        {
+                            'tags': {
+                                '$regex': regex_query
+                            }
+                        }
+                    ]
+                }
+            }
+        )
+    if not dateRange[0] == -1:
+        pipeline.append(
+            {
+                '$match': {
+                    'date': {
+                        '$gte': dateRange[0]
+                    }
+                }
+            }
+        )
+    if not dateRange[1] == -1:
+        pipeline.append(
+            {
+                '$match': {
+                    'date': {
+                        '$lte': dateRange[1]
+                    }
+                }
+            }
+        )
+    if not amountRange[0] == -1:
+        pipeline.append(
+            {
+                '$match': {
+                    'amount': {
+                        '$gte': amountRange[0]
+                    }
+                }
+            }
+        )
+    if not amountRange[1] == -1:
+        pipeline.append(
+            {
+                '$match': {
+                    'amount': {
+                        '$lte': amountRange[1]
+                    }
+                }
+            }
+        )
+    if not len(category) == 0:
+        pipeline.append(
+            {
+                '$match': {
+                    'category': category
+                }
+            }
+        )
+    if sorting == 'date_asc':
+        pipeline.append(
+            {
+                '$sort': {
+                    'date': 1
+                }
+            }
+        )
+    elif sorting == 'date_desc':
+        pipeline.append(
+            {
+                '$sort': {
+                    'date': -1
+                }
+            }
+        )
+    elif sorting == 'amount_asc':
+        pipeline.append(
+            {
+                '$sort': {
+                    'amount': 1
+                }
+            }
+        )
+    elif sorting == 'amount_desc':
+        pipeline.append(
+            {
+                '$sort': {
+                    'amount': -1
+                }
+            }
+        )
+
+    spendingRecords = SpendingRecordModel.SpendingRecord.objects(userId=userId).aggregate(pipeline)
