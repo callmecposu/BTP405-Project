478b13d49e6b206ac3ab8f9300ab0b75f5b20dbb
Jimbert12 <jmanalo11@myseneca.ca>
2024-02-12 21:57:54 -0500

    #client:added login page

---
+++ client/src/pages/login/index.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const Login = () =>{
+
+    return (    
+        <div>
+            login
+        </div>
+    )
+       
+    
+}
+export default Login;
