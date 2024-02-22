a16e73a5f2f9b3450160f622a4faba745e13085a
callmecposu <pc.cposu@gmail.com>
2024-02-22 16:35:10 -0500

    #server: added budgeting to the user model

--- server/models/user.py
+++ server/models/user.py
@@ -1,7 +1,18 @@
 from mongoengine import *
+from enum import Enum
+
+class BudgetType(Enum):
+    DAILY="daily"
+    WEEKLY="weekly"
+    MONTHLY="monthly"
+
+class BudgetSettings(EmbeddedDocument):
+    budget_type = EnumField(BudgetType)
+    max_amount = FloatField()
 
 class User(Document):
     username = StringField(required=True, unique=True)
     password = BinaryField(required=True)
     first_name = StringField(default='New User')
     last_name = StringField(default='')
+    budget = EmbeddedDocumentField(BudgetSettings)
