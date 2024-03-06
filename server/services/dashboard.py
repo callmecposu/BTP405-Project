48ff097bba09c0e0dc726a5951ac76a73e0f8e3b
Vlad Huziienko <guzienko.vlad@gmail.com>
2024-03-06 14:56:44 -0500

    Dashboard finished

--- server/services/dashboard.py
+++ server/services/dashboard.py
@@ -3,6 +3,7 @@ from models import spendingRecord as SpendingRecordModel
 from models import user as UserModel
 import datetime
 from collections import defaultdict
+import calendar
 
 def getCurrentSpendings(userId):
     user = UserModel.User.objects(id=userId).first()
@@ -38,8 +39,12 @@ def getPastSpendings(userId):
                 first_start_date = end_date
             if i == 6:
                 last_end_date = start_date
+
+            start_month = calendar.month_name[int(start_date.split('-')[1])]
             periods.append({
-                'period': f'{start_date} - {end_date}',
+                'period': f'{start_month} {start_date.split("-")[0]}',
+                'start_date': start_date,
+                'end_date': end_date,
                 'spending': {
                     'Grocery': 0,
                     'Transport': 0,
@@ -60,8 +65,15 @@ def getPastSpendings(userId):
                 first_start_date = end_date
             if i == 6:
                 last_end_date = start_date
+
+            start_day = int(start_date.split('-')[2])
+            end_day = int(end_date.split('-')[2])
+            start_month = calendar.month_name[int(start_date.split('-')[1])]
+            end_month = calendar.month_name[int(end_date.split('-')[1])]
             periods.append({
-                'period': f'{start_date} - {end_date}',
+                'period': f'{start_month} {start_day} - {end_month+" " if not start_month == end_month else ""}{end_day}',
+                'start_date': start_date,
+                'end_date': end_date,
                 'spending': {
                     'Grocery': 0,
                     'Transport': 0,
@@ -82,8 +94,12 @@ def getPastSpendings(userId):
                 first_start_date = end_date
             if i == 6:
                 last_end_date = start_date
+
+            start_month = calendar.month_name[int(start_date.split('-')[1])]
             periods.append({
-                'period': f'{start_date} - {end_date}',
+                'period': f"{start_month} {start_date.split('-')[2]}",
+                'start_date': start_date,
+                'end_date': end_date,
                 'spending': {
                     'Grocery': 0,
                     'Transport': 0,
@@ -106,6 +122,8 @@ def getPastSpendings(userId):
                 last_end_date = start_date
             periods.append({
                 'period': f'{start_date} - {end_date}',
+                'start_date': start_date,
+                'end_date': end_date,
                 'spending': {
                     'Grocery': 0,
                     'Transport': 0,
@@ -117,16 +135,18 @@ def getPastSpendings(userId):
                 }
             })
             today = today.replace(day=1) - datetime.timedelta(days=1)
+
     spendings = SpendingRecordModel.SpendingRecord.objects(userId=userId, date__gte=last_end_date, date__lte=first_start_date)
     for spending in spendings:
         period_index = None
         for index, p in enumerate(periods):
-            start_date = datetime.datetime.strptime(p['period'].split(' - ')[0], '%Y-%m-%d')
-            end_date = datetime.datetime.strptime(p['period'].split(' - ')[1], '%Y-%m-%d')
+            start_date = datetime.datetime.strptime(p['start_date'], '%Y-%m-%d')
+            end_date = datetime.datetime.strptime(p['end_date'], '%Y-%m-%d')
             if start_date <= spending.date <= end_date:
                 period_index = index
                 break
 
         if period_index is not None:
             periods[period_index]['spending'][spending.category] += spending.amount
+        
     return periods
