5e6f531f43cd002a1c1046b0a41f942f6f17fec7
callmecposu <pc.cposu@gmail.com>
2024-04-17 20:13:16 -0400

    #server: fixing errors

--- server/services/jwt.py
+++ server/services/jwt.py
@@ -1,10 +1,14 @@
 import jwt
 import os 
+from dotenv import load_dotenv
 
+load_dotenv()
 secret = os.environ.get('JWT_SECRET')
 
 def createToken(id):
     payload = {'id': id}
+    print('payload: ', payload)
+    print('secret: ', secret)
     token = jwt.encode(payload, secret, algorithm='HS256')
     print('Encoded Token: ', token)
     return token
